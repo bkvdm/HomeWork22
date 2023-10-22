0055305d7174f2b579f2f4652eb8bada4eb7e864
bkvdm <vadim@991199.ru>
2023-10-22 15:46:09 +0300

    Домашняя работа №22 (Ссылка на задание: https://skyengpublic.notion.site/2-7-9f1fc1394de0466bba48ae26f801bcb4)

---
+++ src/main/java/tel/bvm/homework22/Homework22Application.java
@@ -0,0 +1,13 @@
+package tel.bvm.homework22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework22Application.class, args);
+	}
+
+}
